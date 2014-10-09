68414f551d148546e8714eb5776576f5c0b64a8f
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-10-09 17:59:32 +1100

    wow

--- gdata_4/get_mds.R
+++ gdata_4/get_mds.R
@@ -1,4 +1,5 @@
 library(cluster)
+
 library(ape)
 library(doParallel)
 
@@ -7,11 +8,11 @@ dat_raw[upper.tri(dat_raw, diag = T)] <- 0
 dat_raw[1:ncol(dat_raw), 1:nrow(dat_raw)] <- as.numeric(dat_raw)
 
 dat_dist <- as.dist(dat_raw)
+
 mds_dat <- cmdscale(dat_dist, k = 2)
 
 write.table(mds_dat, file = 'mds_topo.txt', row.names = T)
 
-stop('getting mds')
 
 ### functions
 
@@ -23,13 +24,13 @@ get_boot_rep <- function(x){
 }
 
 ###
-
+if(T){
 cl <- makeCluster(10)
 registerDoParallel(cl)
 true_dat_clus <- clus_fun_par(mds_dat, 50)
 stopCluster(cl)
 
-if(T){
+
 boot_dat_clus <- list()
 
 cl <- makeCluster(10)
@@ -42,34 +43,35 @@ for(i in 1:50){
 
 
 stopCluster(cl)
+
 }
-pdf('topo_clusters.pdf')
+
+#pdf('topo_clusters.pdf')
 par(mfrow = c(2, 1))
 plot(2:50, as.numeric(true_dat_clus), type = 'l', lwd = 2, col = 'red', ylim = c(0.3, 1), ylab = 'Average silhouette width', xlab = 'Number of topology clusters')
 for(i in 1:length(boot_dat_clus)){
-#      points(jitter(2:50), as.numeric(boot_dat_clus[[i]]), pch = 20, col = rgb(0, 0, 0.5, 0.2))
-      points(jitter(2:50), as.numeric(boot_dat_clus[[i]]), pch = 20, col = 1)
+      points(jitter(2:50), as.numeric(boot_dat_clus[[i]]), pch = 20, col = rgb(0, 0, 0.5, 0.2))
 
 }
-
-
-
 
 clu1 <- pam(mds_dat, k = 5)
 
 plot(mds_dat, pch = 20, col = clu1$clustering, xlab = 'MDS 1', ylab = 'MDS 2')
 
+#dev.off()
+
+
+
 write.table(t(t(clu1$clustering)), file = 'clust_k_5.txt', row.names = T)
 write.table(clu1$clusinfo, file = 'clust_info_k_5.txt', row.names = F)
 
-all_trees <- read.tree('nj_trees.trees')
+#all_trees <- read.tree('nj_trees.trees')
 
-medoid_trees <- all_trees[clu1$id.med]
+#medoid_trees <- all_trees[clu1$id.med]
 
-true_trees <- read.tree('simtree1.tre')
+#true_trees <- read.tree('simtree1.tre')
 
-t_dists <- vector()
-for(i in 1:length(medoid_trees)){
-      t_dists[i] <- dist.topo(medoid_trees[[i]], true_trees[[i]])
-}
-dev.off()
+#t_dists <- vector()
+#for(i in 1:length(medoid_trees)){
+#      t_dists[i] <- dist.topo(medoid_trees[[i]], true_trees[[i]])
+#}
